5cf2e1ebe86599abe77d391ad1ee64ae068684e5
ProPanek <sebek323@gmail.com>
2018-08-19 01:13:55 +0200

    minor fixes

--- src/App.js
+++ src/App.js
@@ -194,7 +194,9 @@ class App extends Component {
 
   handleAchievementBonus = float => {
     let status = Object.assign({}, this.state.status);
-    status.quantityPerClick = float;
+    status.quantityPerClick = float + 1;
+    console.log(status.quantityPerClick);
+
     this.setState({ status: status });
   };
 
