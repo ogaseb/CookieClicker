f8c97c0d5c57423643d8640f4a34364c172e6404
ProPanek <sebek323@gmail.com>
2018-08-23 12:45:14 +0200

    organizing achievements functions

--- src/components/CookieClicker/stateModels/achievementsState.jsx
+++ src/components/CookieClicker/stateModels/achievementsState.jsx
@@ -1,30 +1,22 @@
 const achievements = [
-  [
-    {
-      // Cookies made per hand [0]
-      level: 0
-    }
-  ],
-  [
-    {
-      //alltime cookies [1]
-      level: 0,
-      criteria: 10
-    }
-  ],
-  [
-    {
-      // cookies per second [2]
-      level: 0
-    }
-  ],
-  [
-    {
-      // levels [3]
-      level: 0,
-      criteria: 1
-    }
-  ],
+  {
+    // Cookies made per hand [0]
+    level: 0
+  },
+  {
+    //alltime cookies [1]
+    level: 0,
+    criteria: 10
+  },
+  {
+    // cookies per second [2]
+    level: 0
+  },
+  {
+    // levels [3]
+    level: 0,
+    criteria: 1
+  },
   [
     //upgrades
     {
